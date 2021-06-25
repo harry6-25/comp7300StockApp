d5dbcb3e19799a9ab705caf975ef176068c74453
harry6-25 <harry@LEEdeMBP.lan>
2021-06-25 17:26:39 +0800

    tensorflow

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "There once was a man named George" << endl;
+    cout << "He was 70 years old" << endl;
+    cout << "He liked the name George" << endl;
+    cout << "But did not like being 70" << endl;
+    
+    return 0;
+}
